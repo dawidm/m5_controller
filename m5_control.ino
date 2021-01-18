1f9fe6603b91a166e254b01e886903acaf50855f
Dawid Motyka <dawidmt@gmail.com>
2021-01-18 20:33:51 +0100

    implement bank/preset switching logic

--- m5_control.ino
+++ m5_control.ino
@@ -4,9 +4,15 @@ const int BUTTON_PIN = 2;
 const int button_sampling_ms = 10;
 const int long_press_num_samples = 75;
 
-int last_button_read_millis = 0;
+long last_button_read_millis = 0;
 int button_pressed_samples = 0;
 boolean last_long_pressed = false;
+
+int num_presets = 4;
+int num_banks = 2;
+
+int current_preset = -1; // -1 is initial state
+int current_bank = 0;
 
 
 void setup() {
@@ -17,8 +23,12 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - last_button_read_millis > button_sampling_ms) {
+  
+  if (millis() - last_button_read_millis < 0) // overflow
+    last_button_read_millis = 0;
     
+  if (millis() - last_button_read_millis > button_sampling_ms) {
+        
     boolean button_state = (digitalRead(BUTTON_PIN) == HIGH)?true:false;
     
     if (button_state)
@@ -27,13 +37,13 @@ void loop() {
     if (button_pressed_samples >= long_press_num_samples) {
       button_pressed_samples = 0;
       last_long_pressed = true;
-      longPress();
+      long_press();
     }
     
     if (!button_state) {
       if (button_pressed_samples > 0 & last_long_pressed == false) {
         button_pressed_samples = 0;
-        shortPress();
+        short_press();
       }
       last_long_pressed = false;
       button_pressed_samples = 0;
@@ -44,10 +54,36 @@ void loop() {
 
 }
 
-void longPress() {
-  Serial.println("long press");
+void long_press() {
+  if (current_preset == -1) {
+     current_preset = 0;
+  } else {
+    current_bank++;
+    if (current_bank == num_banks)
+      current_bank = 0;
+    current_preset = 0;
+    Serial.println("bank switch");
+    load_preset();
+  }
 }
 
-void shortPress() {
-  Serial.println("press");
+void short_press() {
+  if (current_preset == -1) {
+     current_preset = 0;
+  } else {
+    current_preset++;
+    if (current_preset == num_presets)
+      current_preset = 0;
+  }
+  load_preset();
+}
+
+void load_preset() {
+  Serial.print("loading preset ");
+  Serial.print(current_preset);
+  Serial.print(" bank ");
+  Serial.print(current_bank);
+  int m5_preset = current_bank * num_presets + current_preset + 1;
+  Serial.print("; m5 preset no ");
+  Serial.println(m5_preset);
 }
